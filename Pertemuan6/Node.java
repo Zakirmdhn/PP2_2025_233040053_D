1fe90f42f99cb6e56d966c08a38605648e693034
Zakirmdhn <125425860+Zakirmdhn@users.noreply.github.com>
2025-03-14 20:19:53 +0700

    P6

---
+++ Pertemuan6/Node.java
@@ -0,0 +1,32 @@
+package Pertemuan6;
+
+public class Node {
+	private double  data;
+	public Node next;
+	
+	//inisialisasi Node
+	public Node(double data) {
+		this.data = data;
+		this.next = null;
+	}
+	
+	//setter and getter
+	public void setData(double data) {
+		this.data = data;
+	}
+
+	public double getData() {
+		return data;
+	}
+	
+	//setter untuk next
+	public void setNext(Node next) {
+        this.next = next;
+    }
+
+	//getter untuk next
+	public Node getNext() {
+        return next;
+    }
+
+}
