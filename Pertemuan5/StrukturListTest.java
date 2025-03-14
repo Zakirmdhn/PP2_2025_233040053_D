655b41fbf85b893caf5b0c5a776156c13a06ff4d
Zakirmdhn <125425860+Zakirmdhn@users.noreply.github.com>
2025-03-14 20:19:16 +0700

    P5

---
+++ Pertemuan5/StrukturListTest.java
@@ -0,0 +1,24 @@
+package Pertemuan5;
+
+public class StrukturListTest {
+	 public static void main(String[] args) {
+	        StrukturList list = new StrukturList();
+
+	        list.addHead(1);
+	        list.addHead(5);
+	        list.addHead(3);
+	        list.addHead(6);
+	        list.addHead(2);
+	        System.out.println("Tampilan list saat ini:");
+	        list.displayElement();
+	        list.removeMid();
+	        System.out.println("Tampilan list saat ini:");
+	        list.displayElement();
+	        // Instruksi tambahan agar output menjadi seperti berikut: 2 1
+	        list.removeMid();
+	        list.removeMid();
+	        System.out.println("Tampilan list saat ini:");
+	        list.displayElement();
+	    }
+
+}
